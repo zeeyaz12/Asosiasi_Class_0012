77a800899f13f91349146f27a57d24db9e6904d2
zeeyaz12 <yumekoae77@gmail.com>
2026-06-05 17:15:14 +0700

    menambahkan class

--- Main.java
+++ Main.java
@@ -14,3 +14,19 @@ abstract class Orang {
     public abstract void tampilkanPeran();
 }
 
+class MataKuliah {
+    private String kode;
+    private String nama;
+    private int sks;
+
+    public MataKuliah(String kode, String nama, int sks) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+    }
+
+    public String getInfo() {
+        return kode + " - " + nama + " (" + sks + " SKS)";
+    }
+}
+
